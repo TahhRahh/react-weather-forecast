52822cce58e642f1fb002e0fe340e6f646b4ca81
TahhRahh <1234tararyan@gmail.com>
2021-08-29 13:29:58 +0100

    background

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div class="bubbles">
       <div className="container">
         <Weather defaultCity="Dublin" />
       </div>
+    </div>
     </div>
   );
 }
